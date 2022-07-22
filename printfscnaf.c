860a92b98b5014e3ec5a5ab090b52fb33d80faf5
JiJoon-Lee <109803442+JiJoon-Lee@users.noreply.github.com>
2022-07-22 21:46:46 +0900

    Add files via upload

---
+++ printfscnaf.c
@@ -0,0 +1,42 @@
+#include <stdio.h> 
+int main(void)
+{
+	//정수형 변수에 대한 예제
+	/*int age = 12;
+	printf("%d\n", age);
+	age = 13;
+	printf("%d\n", age);
+	
+	//실수형 변수에 대한 예제
+	float f = 46.5;
+	printf("%.2f\n", f); //실수형 변수의 소수점 둘째 자리 까지만 출력
+	double d = 4.438;
+	printf("%.2lf\n", d); //소수점 세번째 자리에서 반올림
+
+	//상수
+	const int YEAR = 2000; 
+	printf("태어난 년도 : %d\n", YEAR);
+
+	//printf
+	int add = 3 + 7; //10
+	//printf("3+7= %d\n", add);
+	printf("%d x %d=%d\n", 30, 79, 30 * 79); 
+
+	//scanf
+	//키보드 입력을 받아서 저장
+	int input;
+	printf("값을 입력하세요 : ");
+	scanf_s("%d", &input);
+	printf("입력값 : %d\n", input); 
+
+	int one, two, three;
+	printf("3개의 정수를 입력하세요 : ");
+	scanf_s("%d %d %d", &one, &two, &three);
+	printf("첫 번째 값 : %d\n", one);
+	printf("첫 번째 값 : %d\n", two);
+	printf("첫 번째 값 : %d\n", three ); */
+
+	//문자(한 글자), 문자열
+
+	return 0;
+}
